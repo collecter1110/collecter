3012c1f3d9a8a9f975f0a06d5505cb2b1a32b9e4
irismake <iris3455@gmail.com>
2024-10-12 22:50:27 +0900

    fix : Change background color according to SingleChildScrollView

--- lib/page/collection/collection_detail_screen.dart
+++ lib/page/collection/collection_detail_screen.dart
@@ -43,6 +43,7 @@ class CollectionDetailScreen extends StatelessWidget {
       final CollectionModel _collectionDetail = provider.collectionDetail!;
 
       return Scaffold(
+        backgroundColor: Color(0xFFf8f9fa),
         appBar: CustomAppbar(
           titleText: '컬렉션',
           actionButtonOnTap: () async {
@@ -50,194 +51,224 @@ class CollectionDetailScreen extends StatelessWidget {
           },
           actionButton: 'icon_more',
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(
-                    left: 16.0.w, right: 16.0.w, bottom: 46.0.h),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _collectionDetail.imageFilePath != null
-                        ? Padding(
-                            padding: EdgeInsets.only(top: 16.0.h),
-                            child: AspectRatio(
-                              aspectRatio: 1 / 1,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(8),
-                                ),
-                                child: Container(
-                                  height: 300.h,
-                                  decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                      image: NetworkImage(
-                                          _collectionDetail.imageFilePath!),
-                                      fit: BoxFit.cover,
+        body: DecoratedBox(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.white,
+                Color(0xFFf8f9fa),
+              ],
+              stops: [0.49, 0.51],
+            ),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        left: 16.0.w, right: 16.0.w, bottom: 36.0.h),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _collectionDetail.imageFilePath != null
+                            ? Padding(
+                                padding: EdgeInsets.only(top: 16.0.h),
+                                child: AspectRatio(
+                                  aspectRatio: 1 / 1,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(8),
                                     ),
+                                    child: Container(
+                                      height: 300.h,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          image: NetworkImage(
+                                              _collectionDetail.imageFilePath!),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            : SizedBox.shrink(),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 26.0.h,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                _collectionDetail.title,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 22.sp,
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: FontWeight.w700,
+                                  height: 1.4,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                              ),
+                              likedButton(
+                                collectionId: _collectionDetail.id,
+                                isLiked: _collectionDetail.isLiked!,
+                                likedNum: _collectionDetail.likeNum!,
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10.0.h,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              _collectionDetail.userName,
+                              style: TextStyle(
+                                color: Color(0xFF868e96),
+                                fontSize: 12.sp,
+                                fontFamily: 'Pretendard',
+                                fontWeight: FontWeight.w600,
+                                height: 1.5,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 2.0.w),
+                              child: Image.asset(
+                                'assets/images/image_vertical_line.png',
+                                fit: BoxFit.contain,
+                                color: Color(0xFF868e96),
+                                height: 10.0.h,
+                              ),
+                            ),
+                            Text(
+                              _collectionDetail.createdAt!,
+                              style: TextStyle(
+                                color: Color(0xFF868e96),
+                                fontSize: 12.sp,
+                                fontFamily: 'Pretendard',
+                                fontWeight: FontWeight.w600,
+                                height: 1.5,
+                              ),
+                            ),
+                          ],
+                        ),
+                        _collectionDetail.description != null
+                            ? Padding(
+                                padding: EdgeInsets.symmetric(vertical: 22.0.h),
+                                child: ExpandableText(
+                                    maxLine: 3,
+                                    textStyle: TextStyle(
+                                      color: Color(0xFF343a40),
+                                      fontSize: 14.sp,
+                                      fontFamily: 'Pretendard',
+                                      fontWeight: FontWeight.w500,
+                                      height: 1.43,
+                                    ),
+                                    text: _collectionDetail.description!),
+                              )
+                            : SizedBox.shrink(),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 4.0.h),
+                          child: _collectionDetail.tags != null
+                              ? Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    TagTextStyle(
+                                      tags: _collectionDetail.tags!,
+                                      color: Color(0xFF868E96),
+                                      maxLine: 3,
+                                    ),
+                                  ],
+                                )
+                              : SizedBox.shrink(),
+                        ),
+                        SizedBox(
+                          height: 22.0.h,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 4.0.h),
+                          child: _collectionDetail.primaryKeywords != null
+                              ? Wrap(
+                                  direction: Axis.horizontal,
+                                  alignment: WrapAlignment.start,
+                                  spacing: 5.0.w,
+                                  runSpacing: 5.0.h,
+                                  children: _collectionDetail.primaryKeywords!
+                                      .map((keyword) {
+                                    return Keyword(
+                                        keywordName: keyword.keywordName);
+                                  }).toList(),
+                                )
+                              : SizedBox.shrink(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Color(0xFFf8f9fa),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 12.0.h),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 16.0.w),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'SELECTION',
+                              style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 16.sp,
+                                fontFamily: 'Pretendard',
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      _collectionDetail.selectionNum != 0
+                          ? Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 18.0.w,
+                                vertical: 20.0.h,
+                              ),
+                              child: SelectionWidget(
+                                  routeName: _routeName!,
+                                  collectionId: _collectionDetail.id),
+                            )
+                          : Center(
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(vertical: 200.h),
+                                child: Text(
+                                  '셀렉션이 비어있습니다.',
+                                  style: TextStyle(
+                                    color: Color(0xFF868e96),
+                                    fontSize: 14.sp,
+                                    fontFamily: 'Pretendard',
+                                    fontWeight: FontWeight.w500,
                                   ),
                                 ),
                               ),
                             ),
-                          )
-                        : SizedBox.shrink(),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: 26.0.h,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            _collectionDetail.title,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 22.sp,
-                              fontFamily: 'Pretendard',
-                              fontWeight: FontWeight.w700,
-                              height: 1.4,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 1,
-                          ),
-                          likedButton(
-                            collectionId: _collectionDetail.id,
-                            isLiked: _collectionDetail.isLiked!,
-                            likedNum: _collectionDetail.likeNum!,
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.0.h,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          _collectionDetail.userName,
-                          style: TextStyle(
-                            color: Color(0xFF868e96),
-                            fontSize: 12.sp,
-                            fontFamily: 'Pretendard',
-                            fontWeight: FontWeight.w600,
-                            height: 1.5,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 2.0.w),
-                          child: Image.asset(
-                            'assets/images/image_vertical_line.png',
-                            fit: BoxFit.contain,
-                            color: Color(0xFF868e96),
-                            height: 10.0.h,
-                          ),
-                        ),
-                        Text(
-                          _collectionDetail.createdAt!,
-                          style: TextStyle(
-                            color: Color(0xFF868e96),
-                            fontSize: 12.sp,
-                            fontFamily: 'Pretendard',
-                            fontWeight: FontWeight.w600,
-                            height: 1.5,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 22.0.h,
-                    ),
-                    ExpandableText(
-                        maxLine: 3,
-                        textStyle: TextStyle(
-                          color: Color(0xFF343a40),
-                          fontSize: 14.sp,
-                          fontFamily: 'Pretendard',
-                          fontWeight: FontWeight.w500,
-                          height: 1.43,
-                        ),
-                        text: _collectionDetail.description ?? ''),
-                    SizedBox(
-                      height: 32.0.h,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 4.0.h),
-                      child: _collectionDetail.tags != null
-                          ? Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                TagTextStyle(
-                                  tags: _collectionDetail.tags!,
-                                  color: Color(0xFF868E96),
-                                  maxLine: 3,
-                                ),
-                              ],
-                            )
-                          : SizedBox.shrink(),
-                    ),
-                    SizedBox(
-                      height: 22.0.h,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 4.0.h),
-                      child: _collectionDetail.primaryKeywords != null
-                          ? Wrap(
-                              direction: Axis.horizontal,
-                              alignment: WrapAlignment.start,
-                              spacing: 5.0.w,
-                              runSpacing: 5.0.h,
-                              children: _collectionDetail.primaryKeywords!
-                                  .map((keyword) {
-                                return Keyword(
-                                    keywordName: keyword.keywordName);
-                              }).toList(),
-                            )
-                          : SizedBox.shrink(),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                color: Color(0xFFf8f9fa),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 12.0.h),
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 16.0.w),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            'SELECTION',
-                            style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                              fontSize: 16.sp,
-                              fontFamily: 'Pretendard',
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 18.0.w,
-                        vertical: 20.0.h,
-                      ),
-                      child: SelectionWidget(
-                          routeName: _routeName!,
-                          collectionId: _collectionDetail.id),
-                    ),
-                  ],
-                ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
       );
