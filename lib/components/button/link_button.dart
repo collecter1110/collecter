3b5fc59088885a9b13ad83aafdbc9e648e02071e
irismake <iris3455@gmail.com>
2024-10-13 14:10:18 +0900

    feat : Added shortcut function through link browser

--- lib/components/button/link_button.dart
+++ lib/components/button/link_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class LinkButton extends StatelessWidget {
   final String linkUrl;
@@ -10,19 +12,35 @@ class LinkButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Future<void> _launchInBrowser(String link) async {
+      Uri url = Uri.parse(linkUrl);
+      if (!await launchUrl(
+        url,
+        mode: LaunchMode.externalApplication,
+      )) {
+        throw Exception('Could not launch $url');
+      }
+    }
+
     return Align(
-      alignment: Alignment.centerLeft,
+      alignment: Alignment.center,
       child: TextButton(
-        onPressed: () {},
-        style: TextButton.styleFrom(
+        onPressed: () async {
+          await _launchInBrowser(linkUrl);
+        },
+        style: ElevatedButton.styleFrom(
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(6.0),
+            borderRadius: BorderRadius.circular(4.0.r),
           ),
           minimumSize: Size.zero,
-          padding: EdgeInsets.symmetric(horizontal: 10.0.w, vertical: 2.0.h),
-          backgroundColor: Color(0xFFf1f3f5),
+          padding: EdgeInsets.symmetric(horizontal: 10.0.w, vertical: 3.0.h),
+          backgroundColor: Colors.white,
           elevation: 0,
           tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          side: BorderSide(
+            color: Color(0xFFadb5bd),
+            width: 1.0.w,
+          ),
         ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.start,
