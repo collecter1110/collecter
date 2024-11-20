1294a9d64eb9896c403fe39a1b8755877e7f3f2d
irismake <iris3455@gmail.com>
2024-11-20 22:01:13 +0900

    fix : Fixed error can't recognized emoticons

--- lib/components/ui_kit/text_utils.dart
+++ lib/components/ui_kit/text_utils.dart
@@ -1,5 +1,16 @@
+import 'package:characters/characters.dart';
+
 class TextUtils {
   static String insertZwj(String text) {
-    return text.replaceAllMapped(RegExp(r'(\S)(?=\S)'), (m) => '${m[1]}\u200D');
+    final buffer = StringBuffer();
+
+    for (final character in text.characters) {
+      buffer.write(character);
+      if (character != text.characters.last) {
+        buffer.write('\u200D');
+      }
+    }
+
+    return buffer.toString();
   }
 }
