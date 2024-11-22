782405debb61702627d81cf2b42a2a60563ff902
irismake <iris3455@gmail.com>
2024-11-22 15:15:46 +0900

    fix : Fixed an issue where images continued to load

--- lib/components/widget/image_widget.dart
+++ lib/components/widget/image_widget.dart
@@ -4,7 +4,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 
 import '../../data/services/image_service.dart';
 
-class ImageWidget extends StatelessWidget {
+class ImageWidget extends StatefulWidget {
   final String storageFolderName;
   final String imageFilePath;
   final double boarderRadius;
@@ -17,30 +17,50 @@ class ImageWidget extends StatelessWidget {
   });
 
   @override
+  State<ImageWidget> createState() => _ImageWidgetState();
+}
+
+class _ImageWidgetState extends State<ImageWidget> {
+  Future<String>? _imageUrlFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFuture = ImageService.getFullImageUrl(
+      widget.storageFolderName,
+      widget.imageFilePath,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder<String>(
-      future: ImageService.getFullImageUrl(
-        storageFolderName,
-        imageFilePath,
-      ),
+      future: _imageUrlFuture,
       builder: (context, snapshot) {
-        return CachedNetworkImage(
-          imageUrl: snapshot.data ?? '',
-          imageBuilder: (context, imageProvider) => Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(boarderRadius),
-              border: Border.all(
-                color: Color(0xFFdee2e6),
-                width: 0.5.w,
-              ),
-              image: DecorationImage(
-                image: imageProvider,
-                fit: BoxFit.cover,
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return SizedBox.shrink();
+        } else if (snapshot.hasData) {
+          return CachedNetworkImage(
+            imageUrl: snapshot.data ?? '',
+            imageBuilder: (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(widget.boarderRadius),
+                border: Border.all(
+                  color: Color(0xFFdee2e6),
+                  width: 0.5.w,
+                ),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          errorWidget: (context, url, error) => Center(child: Text('No data')),
-        );
+            errorWidget: (context, url, error) =>
+                Center(child: Text('No data')),
+          );
+        } else {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
       },
     );
   }
