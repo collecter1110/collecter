20e2e52cc44e0d04d4d8758982bc8851e9f77b37
irismake <iris3455@gmail.com>
2024-11-27 22:42:41 +0900

    fix : Edit version information UI/UX

--- lib/page/user_page/setting/app_version_screen.dart
+++ lib/page/user_page/setting/app_version_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../components/ui_kit/custom_app_bar.dart';
 import '../../../data/services/setting_service.dart';
@@ -25,8 +26,51 @@ class AppVersionScreen extends StatelessWidget {
           }
 
           final appInfo = snapshot.data!;
-          return Center(
-            child: Text('현재 버전  ${appInfo["앱 버전"]}'),
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Image.asset(
+                    'assets/images/icon.png',
+                    height: 90.0.h,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0.h,
+              ),
+              Center(
+                child: Text.rich(TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '현재 버전',
+                      style: TextStyle(
+                        fontFamily: 'PretendardRegular',
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                      ),
+                    ),
+                    TextSpan(
+                      text: '  v ${appInfo["앱 버전"]}',
+                      style: TextStyle(
+                        fontFamily: 'PretendardRegular',
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                )),
+              ),
+            ],
           );
         },
       ),
